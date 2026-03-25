b0266b287c5bc12c550c694179f5ef54002515eb
Alexandr Saraev <0Shurick0@gmail.com>
2026-03-25 13:10:30 +0300

    Done lab2

--- Valuator/Program.cs
+++ Valuator/Program.cs
@@ -5,25 +5,28 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
         }
+
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.UseAuthorization();
 
-        app.MapRazorPages();
+        // логирование 
+        app.Use(async (context, next) =>
+        {
+            var port = context.Connection.LocalPort;
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Request handled by port {port} - {context.Request.Path}");
+            await next();
+        });
 
+        app.MapRazorPages();
         app.Run();
     }
 }
